c9f7df759056d7618cf66137d48b6d15ee488554
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-06-29 19:48:22 -0400

    FEAT(1990/2018 data): manually scraped useful data

---
+++ server/api/tempavgfn.js
@@ -0,0 +1,14 @@
+const data = require('./statetemps1990/ma.json')
+
+console.log(data)
+
+const avg = thing => {
+  let sum = 0
+  for (let i = 0; i < thing.length; i++) {
+    sum += thing[i].TAVG
+  }
+  let average = sum / thing.length
+  return average.toFixed(2)
+}
+
+console.log('TEMP:', avg(data))
