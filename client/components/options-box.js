b1afc2a575feee769a749678d304c9bc5d5cb65d
Xavier Olivares <xm.olivares@gmail.com>
2019-06-29 14:16:47 -0400

    chore(optionsbox): testing opacity and functionality

--- client/components/options-box.js
+++ client/components/options-box.js
@@ -1,7 +1,12 @@
 import React, {Component} from 'react'
+import {Box, Button} from '@material-ui/core'
 
 export default class OptionsBox extends Component {
   render() {
-    return <div />
+    return (
+      <Box style={{opacity: 0.1, backgroundColor: 'red', zIndex: 2}}>
+        <h1>Hey, what's up?</h1>
+      </Box>
+    )
   }
 }
